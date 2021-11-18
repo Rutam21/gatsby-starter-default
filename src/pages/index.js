e962b8cdb49f38f32e455a5cb61f4b55f4e2a528
Rutam Prita Mishra <rutamprita@gmail.com>
2021-11-19 01:00:12 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,25 @@ const IndexPage = () => (
     <h1></h1>
     <p>Welcome to my profile</p>
     <p>Please have a look to know more about my job profile.</p>
+     <h2>Black/Dark Table</h2>
+  <p>The .table-dark class adds a black background to the table:</p>            
+  <table class="table table-dark">
+    <thead>
+      <tr>
+        <th>Institution Name</th>
+        <th>Grade</th>
+        <th>Percentage</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>C.V. Raman College of Engineering</td>
+        <td>B.Tech, Computer Science and Engineering</td>
+        <td>7.25</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
